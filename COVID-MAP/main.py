148a36aeb787cdae71e165c26257e6984e553993
Kunal Mukherjee <kunmukh@gmail.com>
2020-03-13 00:25:39 -0500

    main file created, only skeleton

---
+++ COVID-MAP/main.py
@@ -0,0 +1,13 @@
+# Kunal Mukherjee
+# COVID MAP
+# 3/13/20 - Friday the 13th
+# Dataset: 2019 Novel Coronavirus COVID-19 (2019-nCoV) Data Repository by Johns Hopkins CSSE
+# https://github.com/CSSEGISandData/COVID-19
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
